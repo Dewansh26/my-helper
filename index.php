28adf3b9d1b93958b08d39ad2a93fa391d7b2f35
unknown <dewanshmishra01@gmail.com>
2023-08-05 18:18:59 +0530

    new changes

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="css/all.min.css">
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/custom.css">
+
+  <title>MYHELPER</title>
+</head>
+
+<body>
+  <!-- Start Navigation -->
+  <nav class="navbar navbar-expand-sm navbar-dark bg-danger pl-5 fixed-top">
+    <a href="index.php" class="navbar-brand">myhelper</a>
+    <span class="navbar-text">Customer's Happiness is our Aim</span>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myMenu">
+      <ul class="navbar-nav pl-5 custom-nav">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#Services" class="nav-link">Services</a></li>
+        <li class="nav-item"><a href="#registration" class="nav-link">Registration</a></li>
+        <li class="nav-item"><a href="Requester/RequesterLogin.php" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="#Contact" class="nav-link">Contact</a></li>
+      </ul>
+    </div>
+  </nav> <!-- End Navigation -->
+
+  <!-- Start Header Jumbotron-->
+  <header class="jumbotron back-image" style="background-image: url(images/9.jpg);">
+    <div class="myclass mainHeading">
+      <h1 class="text-uppercase text-danger font-weight-bold">Welcome to myhelper</h1>
+      <p class="font-italic">Customer's Happiness is our Aim</p>
+      <a href="Requester/RequesterLogin.php" class="btn btn-success mr-4">Login</a>
+      <a href="#registration" class="btn btn-danger mr-4">Sign Up</a>
+    </div>
+  </header> <!-- End Header Jumbotron -->
+
+  <div class="container">
+    <!--Introduction Section-->
+    <div class="jumbotron">
+      <h3 class="text-center">Our Services</h3>
+      <p>
+      The on-demand home services bridges the gap between real world services and instant online services to provide efficient services.
+      The on demand home services system is incredibly useful for everybody who want to urge home services 
+      A home services business is really any business that is concerned with the upkeep or improvement of someone’s home.
+      This encompasses a wide range of industries, some extremely specialized. 
+      Most home services fall into one of three categories: maintenance, repairs, or renovation/redecorating.
+      Renovation/redecorating would include deck and patio companies, kitchen and bath remodelers, and custom window coverings.
+      </p>
+
+    </div>
+  </div>
+  <!--Introduction Section End-->
+  <!-- Start Services -->
+  <div class="container text-center border-bottom" id="Services">
+    <h2>Our Services</h2>
+    <div class="row mt-4">
+      <div class="col-sm-3 mb-4">
+        <a href="#"><i class=" fa fa-wrench fa-8x text-success"></i></a>
+        <h4 class="mt-4">Plumber</h4>
+      </div>
+      <div class="col-sm-3 mb-4">
+        <a href="#"><i class="fas fa-toolbox fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Electrician</h4>
+      </div>
+      <div class="col-sm-3 mb-4">
+        <a href="#"><i class="fas fa-trash fa-8x text-info"></i></a>
+        <h4 class="mt-4">Home cleaner</h4>
+      </div>
+      <div class="col-sm-3 mb-4">
+        <a href="#"><i class="fas fa-mobile fa-8x " style="color: black;"></i></a>
+        <h4 class="mt-4">Mobile Repairer</h4>
+      </div>
+    </div>
+  </div> <!-- End Services -->
+
+  <!-- Start Registration  -->
+  <?php include('userRegistration.php') ?>
+  <!-- End Registration  -->
+
+ 
+
+  <!--Start Contact Us-->
+  <div class="container" id="Contact">
+    <!--Start Contact Us Container-->
+    <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
+    <div class="row">
+
+      <!--Start Contact Us Row-->
+      <?php include('contactform.php'); ?>
+      <!-- End Contact Us 1st Column -->
+
+      <div class="col-md-4 text-center">
+        <!-- Start Contact Us 2nd Column-->
+        <!-- <strong>Headquarter:</strong> <br>
+        OSMS Pvt Ltd, <br>
+        Sec IV, Bokaro <br>
+        Jharkhand - 834005 <br>
+        Phone: +00000000 <br>
+        <a href="#" target="_blank">www.osms.com</a> <br>
+
+        <br><br>
+        <strong>Delhi Branch:</strong> <br>
+        OSMS Pvt Ltd, <br>
+        Ashok Nagar, Delhi <br>
+        Delhi - 804002 <br>
+        Phone: +00000000 <br>
+        <a href="#" target="_blank">www.osms.com</a> <br> -->
+      </div> <!-- End Contact Us 2nd Column-->
+    </div> <!-- End Contact Us Row-->
+  </div> <!-- End Contact Us Container-->
+  <!-- End Contact Us -->
+
+  <!-- Start Footer-->
+  <footer class="container-fluid bg-dark text-white mt-5" style="border-top: 3px solid #DC3545;">
+    <div class="container">
+      <!-- Start Footer Container -->
+      <div class="row py-3">
+        <!-- Start Footer Row -->
+        <div class="col-md-6">
+          <!-- Start Footer 1st Column -->
+          <!-- <span class="pr-2">Follow Us: </span> -->
+          <!-- <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-twitter"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-youtube"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-google-plus-g"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fas fa-rss"></i></a> -->
+        </div> <!-- End Footer 1st Column -->
+
+        <div class="col-md-6 text-right">
+          <!-- Start Footer 2nd Column -->
+          <small> 
+          </small>
+          <small class="ml-2"><a href="Admin/login.php">Admin Login</a></small>
+        </div> <!-- End Footer 2nd Column -->
+      </div> <!-- End Footer Row -->
+    </div> <!-- End Footer Container -->
+  </footer> <!-- End Footer -->
+
+  <!-- Boostrap JavaScript -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/all.min.js"></script>
+</body>
+
+</html>
